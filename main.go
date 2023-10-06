9143e0460e5bdb2faad5c9bcc636fe822297fda9
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-06 13:32:03 +0530

    Added cobra and initialized the project

---
+++ main.go
@@ -0,0 +1,9 @@
+// get-ip is a command line application that is used to get the ip address
+// assigned to the interface that can connect to the internet
+package main
+
+import "github.com/VinukaThejana/get-ip/cmd"
+
+func main() {
+	cmd.Execute()
+}
